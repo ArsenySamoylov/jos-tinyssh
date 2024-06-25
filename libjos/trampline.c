203c5edaf79c98bc51c98c71270ea78e82dc6a40
ArsenySamoylov <samoylov@ispras.ru>
2024-06-25 23:45:48 +0300

    helloWorld in trampline

--- libjos/trampline.c
+++ libjos/trampline.c
@@ -1,6 +1,8 @@
 int main(int argc, char** argv);
+int cprintf(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
 
 void
 umain(int argc, char** argv) {
+    cprintf("Hello world!\n");
     main(argc, argv);
 }
